6dd71b72f7886387417208620b8411b9415c0a97
Karyme Bautista <74372462+karyme-hub@users.noreply.github.com>
2022-02-08 21:09:02 -0600

    Create Cliente.java

---
+++ SimulacionColas/Cliente.java
@@ -0,0 +1,39 @@
+public class Cliente {
+    String numeroCta;
+    String nombre;
+    int prioridad;
+    public Cliente(String  numeroCta, String nombre, int prioridad){
+        this.nombre=nombre;
+        this.numeroCta=numeroCta;
+        //numero 1= VIP 
+        //numero 2= normal
+        this.prioridad=prioridad;
+    }
+
+    //setters and getters
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getNumeroCta(){
+        return this.numeroCta;
+    }
+    public int getPrioridad(){
+        return this.prioridad;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    } 
+    public void setNumeroCta(String numeroCta){
+        this.numeroCta=numeroCta;
+    } 
+    public void setPrioridad(int prioridad){
+        this.prioridad=prioridad;
+    } 
+
+    @Override
+    public String toString() {
+        return "Nombre: "+this.nombre +"\nNo. Cuenta: "+ this.numeroCta + "\nPrioridad: "+ this.prioridad+"\n--------------------------";
+    }
+    
+}
